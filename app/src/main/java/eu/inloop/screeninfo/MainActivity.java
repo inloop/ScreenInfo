5a5df63d554ac486ff6cabf845206fca8fbb6753
Miroslav Michalec <michalec.miroslav@gmail.com>
2015-09-14 22:16:25 +0200

    Initial commit

---
+++ app/src/main/java/eu/inloop/screeninfo/MainActivity.java
@@ -0,0 +1,145 @@
+package eu.inloop.screeninfo;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int UI_ANIMATION_DELAY = 300;
+
+    private View mContentView;
+    private boolean mVisible;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mVisible = true;
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= 16) {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
+        mContentView = findViewById(R.id.fullscreen_content);
+
+        if (Build.VERSION.SDK_INT > 10) {
+            mContentView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toggle();
+                }
+            });
+        }
+
+        CheckBox toggleAB = (CheckBox) findViewById(R.id.toggleAB);
+        toggleAB.setChecked(true);
+        toggleAB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                ActionBar actionBar = getSupportActionBar();
+                if (actionBar != null) {
+                    if (isChecked) {
+                        actionBar.show();
+                    } else {
+                        actionBar.hide();
+                    }
+                }
+            }
+        });
+
+        float density = getResources().getDisplayMetrics().density;
+        TextView densityNumber = (TextView) findViewById(R.id.densityNumber);
+        densityNumber.setText("(" + Float.toString(density) + ")");
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT > 10) {
+            delayedHide(200);
+        }
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        mVisible = false;
+
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            int flags = mContentView.getSystemUiVisibility();
+            flags = setFlag(flags, View.SYSTEM_UI_FLAG_FULLSCREEN);
+            flags = setFlag(flags, View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            mContentView.setSystemUiVisibility(flags);
+        }
+    };
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        int flags = mContentView.getSystemUiVisibility();
+        flags = setFlag(flags, View.SYSTEM_UI_FLAG_FULLSCREEN);
+        flags = resetFlag(flags, View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        mContentView.setSystemUiVisibility(flags);
+
+        mVisible = true;
+
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+    }
+
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    private int setFlag(int flags, int flag) {
+        if ((flags & flag) == 0) {
+            flags |= flag;
+        }
+        return flags;
+    }
+
+    private int resetFlag(int flags, int flag) {
+        if ((flags & flag) == flag) {
+            flags &= ~flag;
+        }
+        return flags;
+    }
+
+}
---
+++ app/src/main/java/eu/inloop/screeninfo/view/WidthBox.java
@@ -0,0 +1,84 @@
+package eu.inloop.screeninfo.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by miroslavmichalec on 14/09/15.
+ */
+public class WidthBox extends View {
+
+    private int mWidth;
+    private int mWidthDp;
+    private int mHeight;
+    private int mHeightDp;
+
+    private Paint mPaint;
+
+    private float mDensity;
+
+    public WidthBox(Context context) {
+        super(context);
+        init();
+    }
+
+    public WidthBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public WidthBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mDensity = getContext().getResources().getDisplayMetrics().density;
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeWidth(2);
+        mPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        mPaint.setColor(Color.BLUE);
+        canvas.drawRect(10 * mDensity, 10 * mDensity, 110 * mDensity, 110 * mDensity, mPaint);
+        mPaint.setTextSize(12 * mDensity);
+        mPaint.setColor(Color.WHITE);
+        canvas.drawText("100 dp x 100 dp", 58 * mDensity, 65 * mDensity, mPaint);
+
+        mPaint.setTextSize(16 * mDensity);
+        mPaint.setColor(Color.BLUE);
+        canvas.drawLine(mWidth - 50 * mDensity, 0, mWidth - 50 * mDensity, mHeight, mPaint);
+        canvas.drawLine(0, mHeight - 50 * mDensity, mWidth, mHeight - 50 * mDensity, mPaint);
+
+        canvas.save();
+        canvas.rotate(270, mWidth - 55 * mDensity, mHeight / 2);
+        canvas.drawText(
+                Integer.toString(mHeight) + " px (" + Integer.toString(mHeightDp) + " dp)",
+                mWidth - 55 * mDensity, mHeight / 2, mPaint);
+        canvas.restore();
+
+        canvas.drawText(
+                Integer.toString(mWidth) + " px (" + Integer.toString(mWidthDp) + " dp)",
+                mWidth / 2, mHeight  - 55 * mDensity, mPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mWidth = w;
+        mHeight = h;
+        mWidthDp = (int)(w / mDensity);
+        mHeightDp = (int)(h / mDensity);
+    }
+
+}
