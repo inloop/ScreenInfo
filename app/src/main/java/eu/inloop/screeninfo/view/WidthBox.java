5a5a4b5314e2585aefeca556c6e7cfc03521e2ff
Miroslav Michalec <michalec.miroslav@gmail.com>
2015-09-25 22:48:34 +0200

    minor code polishing (constants & dimensions)

--- app/src/main/java/eu/inloop/screeninfo/view/WidthBox.java
+++ app/src/main/java/eu/inloop/screeninfo/view/WidthBox.java
@@ -12,6 +12,20 @@ import android.view.View;
  */
 public class WidthBox extends View {
 
+    private static final int SQUARE_DP = 100;  //dp
+    private static final int SQUARE_PX = 100;  //px
+
+    private static final int TOP_MARGIN_DP = 10;
+
+    private static final int FONT_SIZE_SMALL = 12;
+    private static final int FONT_SIZE_BIG = 16;
+
+    private static final int DP_SQUARE_LEFT_MARGIN_DP = 10;
+    private static final int PX_SQUARE_LEFT_MARGIN_DP = 150;
+
+    private static final int DIMEN_LINE_MARGIN = 50;
+    private static final int DIMEN_LINE_DESC_MARGIN = 55;
+
     private int mWidth;
     private int mWidthDp;
     private int mHeight;
@@ -49,32 +63,63 @@ public class WidthBox extends View {
         super.onDraw(canvas);
 
         mPaint.setColor(Color.BLUE);
-        canvas.drawRect(10 * mDensity, 10 * mDensity, 110 * mDensity, 110 * mDensity, mPaint);
+        canvas.drawRect(
+                DP_SQUARE_LEFT_MARGIN_DP * mDensity,
+                TOP_MARGIN_DP * mDensity,
+                (DP_SQUARE_LEFT_MARGIN_DP + SQUARE_DP) * mDensity,
+                (TOP_MARGIN_DP + SQUARE_DP) * mDensity,
+                mPaint);
 
-        canvas.drawRect(150 * mDensity, 10 * mDensity, (150 * mDensity) + 100, (10 * mDensity) + 100, mPaint);
+        canvas.drawRect(
+                PX_SQUARE_LEFT_MARGIN_DP * mDensity,
+                TOP_MARGIN_DP * mDensity,
+                (PX_SQUARE_LEFT_MARGIN_DP * mDensity) + SQUARE_PX,
+                (TOP_MARGIN_DP * mDensity) + SQUARE_PX,
+                mPaint);
 
-        mPaint.setTextSize(12 * mDensity);
+        mPaint.setTextSize(FONT_SIZE_SMALL * mDensity);
         mPaint.setColor(Color.WHITE);
-        canvas.drawText("100 dp x 100 dp", 58 * mDensity, 65 * mDensity, mPaint);
+        canvas.drawText(
+                Integer.toString(SQUARE_DP) + " dp x " + Integer.toString(SQUARE_DP) + " dp",
+                (DP_SQUARE_LEFT_MARGIN_DP + SQUARE_DP / 2) * mDensity,
+                (TOP_MARGIN_DP + SQUARE_DP / 2) * mDensity,
+                mPaint);
 
         mPaint.setColor(Color.BLACK);
-        canvas.drawText("100 px x 100 px", 150 * mDensity + 50, 30 * mDensity + 100, mPaint);
+        canvas.drawText(
+                Integer.toString(SQUARE_PX) + " px x " + Integer.toString(SQUARE_PX) + " px",
+                (PX_SQUARE_LEFT_MARGIN_DP * mDensity) + SQUARE_PX / 2,
+                (30 * mDensity) + SQUARE_PX,
+                mPaint);
 
-        mPaint.setTextSize(16 * mDensity);
+        mPaint.setTextSize(FONT_SIZE_BIG * mDensity);
         mPaint.setColor(Color.BLUE);
-        canvas.drawLine(mWidth - 50 * mDensity, 0, mWidth - 50 * mDensity, mHeight, mPaint);
-        canvas.drawLine(0, mHeight - 50 * mDensity, mWidth, mHeight - 50 * mDensity, mPaint);
+        canvas.drawLine(
+                mWidth - (DIMEN_LINE_MARGIN * mDensity),
+                0,
+                mWidth - (DIMEN_LINE_MARGIN * mDensity),
+                mHeight,
+                mPaint);
+        canvas.drawLine(
+                0,
+                mHeight - (DIMEN_LINE_MARGIN * mDensity),
+                mWidth,
+                mHeight - (DIMEN_LINE_MARGIN * mDensity), mPaint);
 
         canvas.save();
-        canvas.rotate(270, mWidth - 55 * mDensity, mHeight / 2);
+        canvas.rotate(270, mWidth - (DIMEN_LINE_DESC_MARGIN * mDensity), mHeight / 2);
         canvas.drawText(
                 Integer.toString(mHeight) + " px (" + Integer.toString(mHeightDp) + " dp)",
-                mWidth - 55 * mDensity, mHeight / 2, mPaint);
+                mWidth - (DIMEN_LINE_DESC_MARGIN * mDensity),
+                mHeight / 2,
+                mPaint);
         canvas.restore();
 
         canvas.drawText(
                 Integer.toString(mWidth) + " px (" + Integer.toString(mWidthDp) + " dp)",
-                mWidth / 2, mHeight  - 55 * mDensity, mPaint);
+                mWidth / 2,
+                mHeight  - (DIMEN_LINE_DESC_MARGIN * mDensity),
+                mPaint);
     }
 
     @Override
